f08dace16cf67535e3000828df9890b2a3b53c50
Frieder Pankratz <pankratz@in.tum.de>
2012-08-16 12:03:11 +0200

    add script for adding submodules

---
+++ misc/setup/windows/addStandardModules.bat
@@ -0,0 +1,8 @@
+git submodule add git+ssh://git@arlabnavab1.informatik.tu-muenchen.de:3000/ubitrack/utcore.git modules/utcore
+git submodule add git+ssh://git@arlabnavab1.informatik.tu-muenchen.de:3000/ubitrack/utvision.git modules/utvision
+git submodule add git+ssh://git@arlabnavab1.informatik.tu-muenchen.de:3000/ubitrack/utcomponents.git modules/utcomponents
+git submodule add git+ssh://git@arlabnavab1.informatik.tu-muenchen.de:3000/ubitrack/utvisioncomponents.git modules/utvisioncomponents
+git submodule add git+ssh://git@arlabnavab1.informatik.tu-muenchen.de:3000/ubitrack/utdataflow.git modules/utdataflow
+git submodule add git+ssh://git@arlabnavab1.informatik.tu-muenchen.de:3000/ubitrack/utfacade.git modules/utfacade
+git submodule add git+ssh://git@arlabnavab1.informatik.tu-muenchen.de:3000/ubitrack/utvisualization.git modules/utvisualization
+git submodule add git+ssh://git@arlabnavab1.informatik.tu-muenchen.de:3000/ubitrack/mswindows.git modules/mswindows
