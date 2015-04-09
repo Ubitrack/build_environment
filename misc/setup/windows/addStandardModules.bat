c2325dc5460acd78f2d0d031346aeb11c54daf20
Frieder Pankratz <pankratz@in.tum.de>
2015-04-09 15:08:49 +0200

    change setup scripts to new server

--- misc/setup/windows/addStandardModules.bat
+++ misc/setup/windows/addStandardModules.bat
@@ -1,8 +1,8 @@
-git submodule add git+ssh://git@arlabnavab1.informatik.tu-muenchen.de:3000/ubitrack/utcore.git modules/utcore
-git submodule add git+ssh://git@arlabnavab1.informatik.tu-muenchen.de:3000/ubitrack/utvision.git modules/utvision
-git submodule add git+ssh://git@arlabnavab1.informatik.tu-muenchen.de:3000/ubitrack/utcomponents.git modules/utcomponents
-git submodule add git+ssh://git@arlabnavab1.informatik.tu-muenchen.de:3000/ubitrack/utvisioncomponents.git modules/utvisioncomponents
-git submodule add git+ssh://git@arlabnavab1.informatik.tu-muenchen.de:3000/ubitrack/utdataflow.git modules/utdataflow
-git submodule add git+ssh://git@arlabnavab1.informatik.tu-muenchen.de:3000/ubitrack/utfacade.git modules/utfacade
-git submodule add git+ssh://git@arlabnavab1.informatik.tu-muenchen.de:3000/ubitrack/utvisualization.git modules/utvisualization
-git submodule add git+ssh://git@arlabnavab1.informatik.tu-muenchen.de:3000/ubitrack/mswindows.git modules/mswindows
+git submodule add git+ssh://git@intern.far.in.tum.de:Ubitrack/utCore.git modules/utcore
+git submodule add git+ssh://git@intern.far.in.tum.de:Ubitrack/utVision.git modules/utvision
+git submodule add git+ssh://git@intern.far.in.tum.de:Ubitrack/utComponents.git modules/utcomponents
+git submodule add git+ssh://git@intern.far.in.tum.de:Ubitrack/utVisionComponents.git modules/utvisioncomponents
+git submodule add git+ssh://git@intern.far.in.tum.de:Ubitrack/utDataflow.git modules/utdataflow
+git submodule add git+ssh://git@intern.far.in.tum.de:Ubitrack/utFacade.git modules/utfacade
+git submodule add git+ssh://git@intern.far.in.tum.de:Ubitrack/utVisualization.git modules/utvisualization
+git submodule add git+ssh://git@intern.far.in.tum.de:UbitrackContrib/DirectShowDriver.git modules/mswindows
