22b3aef38cbbdc5f5c7a5e2b216ad8e5694c6299
Frieder Pankratz <pankratz@in.tum.de>
2015-05-07 14:55:55 +0200

    correct new server repo paths

--- misc/setup/windows/addStandardModules.bat
+++ misc/setup/windows/addStandardModules.bat
@@ -1,8 +1,8 @@
-git submodule add git+ssh://git@intern.far.in.tum.de:Ubitrack/utCore.git modules/utcore
-git submodule add git+ssh://git@intern.far.in.tum.de:Ubitrack/utVision.git modules/utvision
-git submodule add git+ssh://git@intern.far.in.tum.de:Ubitrack/utComponents.git modules/utcomponents
-git submodule add git+ssh://git@intern.far.in.tum.de:Ubitrack/utVisionComponents.git modules/utvisioncomponents
-git submodule add git+ssh://git@intern.far.in.tum.de:Ubitrack/utDataflow.git modules/utdataflow
-git submodule add git+ssh://git@intern.far.in.tum.de:Ubitrack/utFacade.git modules/utfacade
-git submodule add git+ssh://git@intern.far.in.tum.de:Ubitrack/utVisualization.git modules/utvisualization
-git submodule add git+ssh://git@intern.far.in.tum.de:UbitrackContrib/DirectShowDriver.git modules/mswindows
+git submodule add git@intern.far.in.tum.de:Ubitrack/utCore.git modules/utcore
+git submodule add git@intern.far.in.tum.de:Ubitrack/utVision.git modules/utvision
+git submodule add git@intern.far.in.tum.de:Ubitrack/utComponents.git modules/utcomponents
+git submodule add git@intern.far.in.tum.de:Ubitrack/utVisionComponents.git modules/utvisioncomponents
+git submodule add git@intern.far.in.tum.de:Ubitrack/utDataflow.git modules/utdataflow
+git submodule add git@intern.far.in.tum.de:Ubitrack/utFacade.git modules/utfacade
+git submodule add git@intern.far.in.tum.de:Ubitrack/utVisualization.git modules/utvisualization
+git submodule add git@intern.far.in.tum.de:UbitrackContrib/DirectShowDriver.git modules/mswindows
